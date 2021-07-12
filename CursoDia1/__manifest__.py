4082c8b9d410a198cd9d2b380becd3e950771e10
Eneas Armas <earmas@crucialsoft.com.mx>
2021-07-12 16:45:53 +0000

    segundo intento

--- CursoDia1/__manifest__.py
+++ CursoDia1/__manifest__.py
@@ -1,19 +1,5 @@
 # -*- coding: utf-8 -*-
 {
     'name' : 'odoo dia 1',
-    'summary' : """Dia uno entrenamiento""",
-    'description' :"""
-        Curso Dia 1 de inicio
-    """,
-    'author' : 'Crucialsoft',
-    'category' : 'Training',
-    'version' : '0.0.1',
-    'depends' : ['base']
-    'data' : [
-        
-    ],
-    'demo': [
-        
-    ],
 
 }
