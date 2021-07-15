7613f6d04e00e3e7fe7f47bad98b277e5149d1f1
Eneas <earmas@crucialsoft.com.mx>
2021-07-15 14:55:17 +0000

    Curso primeros tres dias

--- CursoDia1/.ipynb_checkpoints/__manifest__-checkpoint.py
+++ CursoDia1/.ipynb_checkpoints/__manifest__-checkpoint.py
@@ -1,19 +1,21 @@
 # -*- coding: utf-8 -*-
 {
     'name' : 'odoo dia 1',
-    'summary' : """Dia uno entrenamiento""",
+    'summary' : """Dia uno entrenamiento 1255""",
     'description' :"""
-        Curso Dia 1 de inicio
+        Curso Dia 1 de inicio modificado 1255
     """,
     'author' : 'Crucialsoft',
     'category' : 'Training',
     'version' : '0.0.1',
     'depends' : ['base'],
     'data' : [
-        
+        'security/academia_seguridad.xml',
+        'security/ir.model.access.csv',
+        'views/curso_vista.xml',
+        'views/academia_menu.xml',
     ],
-    'demo': [
-        
+    'demo' : [
+        'demo/dato_ini.xml',
     ],
-
 }
