5a2f9e3f47c77f2477bdaaff29e7d9ed99b3db7d
Eneas Armas <earmas@crucialsoft.com.mx>
2021-07-19 11:44:13 -0500

    Se agrega vista pivote y calendario

--- CursoDia1/models/session.py
+++ CursoDia1/models/session.py
@@ -16,3 +16,30 @@ class Session(models.Model):
     instructor_id = fields.Many2one(comodel_name='res.partner',string='Instructor')
     
     students_ids = fields.Many2many(comodel_name='res.partner',string='Estudiantes')
+    
+    start_date=fields.Date(string='Fecha Inicio',default=fields.Date.today)
+    
+    duration = field.Integer(string='Dias de sesión',default = 1)
+    
+    end_date=fields.Date(string='Fecha Fin'
+                        ,compute='_compute_end_date'
+                        ,inverse='_inverse_end_date'
+                        ,store=True)
+                        
+    @api.depends('start_date','end_date')
+    def _compute_end_date(self):
+        for record in self:
+            if not (record.start_date and record.duration):
+                record.end_date = record.start_date
+            else:
+                duration = timedelta(day=record.duration)
+                record.end_date = record.start_date + duration
+                
+    def _inverse_end_date(self):
+        for record in self:
+            if record.start_date and record.end_date:
+                record.duration = (record.end_date - record.start_date).days + 1
+            else:
+                continue
+                
+                
