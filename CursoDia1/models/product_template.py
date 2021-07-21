758a69e85916bb1c9ff2bfefaf62a7c0dae926cb
Eneas Armas <earmas@crucialsoft.com.mx>
2021-07-21 12:24:49 -0500

    se corrigen comas

--- CursoDia1/models/product_template.py
+++ CursoDia1/models/product_template.py
@@ -6,5 +6,5 @@ class ProductTemplate(models.Model):
     _inherit='product.template'
     
     is_session_product = fields.Boolean(string='Usar como sesion de producto'
-                                        help='Usa esta caja como sesion de seleccion de productos'
-                                        default=False)
+                                        ,help='Usa esta caja como sesion de seleccion de productos'
+                                        ,default=False)
