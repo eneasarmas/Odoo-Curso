eb854b5314577e42578d24e11e5d6ecba3c293a8
Eneas Armas <earmas@crucialsoft.com.mx>
2021-07-21 10:43:14 -0500

    se corrige igual

--- CursoDia1/models/sale_order.py
+++ CursoDia1/models/sale_order.py
@@ -5,7 +5,7 @@ from odoo import models,fields,api
 class SaleOrder(models.Model):
     _inherit = 'sale.order'
     
-    session_id = = fields.Many2one(comodel_name='academy.session'
+    session_id = fields.Many2one(comodel_name='academy.session'
                             ,string='Sesion Relacion'
                             ,ondelete='set null')
 
