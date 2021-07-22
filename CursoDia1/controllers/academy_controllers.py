1cbed14b470f8f2abec89f11943dbd6c769f26a1
Eneas Armas <earmas@crucialsoft.com.mx>
2021-07-22 15:53:20 -0500

    se actualiza dato

--- CursoDia1/controllers/academy_controllers.py
+++ CursoDia1/controllers/academy_controllers.py
@@ -10,13 +10,13 @@ class Academy(http.Controller):
     @http.route('/academy/courses/',auth='public', website=True)
     def courses(self,**kw):
         courses = http.request.env['academy.course'].search([])
-        return http.request.render('CursoDia1.course_website'
+        return http.request.render('odoo_academy.course_website'
             ,{'courses':courses}
         )
     
     @http.route('/academy/<model("academy.session"):session>/',auth='public', website=True)
     def session(self,session):
-        return http.request.render('CursoDia1.session_website'
+        return http.request.render('odoo_academy.session_website'
             ,{'session':session}
         )
 
