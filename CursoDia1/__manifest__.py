c9586af0870939950fa21cf28562ae8ede26d05f
Eneas Armas <earmas@crucialsoft.com.mx>
2021-07-22 12:27:38 -0500

    se agrega controlador de pagina web

--- CursoDia1/__manifest__.py
+++ CursoDia1/__manifest__.py
@@ -8,7 +8,7 @@
     'author' : 'crucialsoft',
     'category' : 'Training',
     'version' : '0.0.1',
-    'depends' : ['sale','web_cohort'],
+    'depends' : ['sale','web_cohort','website'],
     'data' : [
         'security/academy_security.xml',
         'security/ir.model.access.csv',
@@ -20,6 +20,7 @@
         'views/product_views_inherit.xml',
         'wizard/sale_wizard_view.xml',
         'report/session_report_templates.xml',
+        'views/academy_web_templates.xml',
     ],
     'demo': [
         'demo/academy_demo.xml',
